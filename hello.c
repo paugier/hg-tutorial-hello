64c142095850a87820d67ab9613b5f33070291f9
mpm@selenic.com <mpm@selenic.com>
2005-08-26 01:20:50 -0700

    Create a standard "hello, world" program

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+ * hello.c
+ *
+ * Placed in the public domain by Bryan O'Sullivan
+ *
+ * This program is not covered by patents in the United States or other
+ * countries.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello, world!\n");
+	return 0;
+}
