5edab901b8f31b6f9808d112f478043639d8f39d
Bryan O'Sullivan <bos@serpentine.com>
2008-08-16 22:16:53 +0200

    Trim comments.

--- hello.c
+++ hello.c
@@ -1,10 +1,6 @@
 /*
- * hello.c
- *
- * Placed in the public domain by Bryan O'Sullivan
- *
- * This program is not covered by patents in the United States or other
- * countries.
+ * Placed in the public domain by Bryan O'Sullivan.  This program is
+ * not covered by patents in the United States or other countries.
  */
 
 #include <stdio.h>
